36eaaf122736c858e3563730629e316f93a1b151
JulianStudent1 <jurodrigueza@uniempresarial.edu.co>
2023-07-23 14:47:33 -0500

    correccion

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
-const app_module_1 = require("./app.module");
+const app_module_1 = require("./src/app.module");
 const platform_fastify_1 = require("@nestjs/platform-fastify");
 const fmp = require("@fastify/multipart");
 const fsf = require("@fastify/static");
