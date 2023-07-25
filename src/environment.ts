f67797f651ea927a6ea856a1bd507e998966c603
Juan Miguel Castro Rojas <107083834+jucastro94@users.noreply.github.com>
2023-07-24 20:55:28 -0500

    Update environment.ts
    modificacion cadena de conexion

--- src/environment.ts
+++ src/environment.ts
@@ -4,6 +4,6 @@ export default () => ({
 
     token_key: 'no-rubrics?',
 
-    db_string_connection: "mongodb+srv://jurodrigueza:3aXc0zvypcDwjBus@mobilesappsii.z3wgrih.mongodb.net/mobilesappsii?retryWrites=true&w=majority",
-
+    //db_string_connection: "mongodb+srv://jurodrigueza:3aXc0zvypcDwjBus@mobilesappsii.z3wgrih.mongodb.net/mobilesappsii?retryWrites=true&w=majority",
+    db_string_connection: "mongodb+srv://jucastro:Paris-5678@juancluster.y9bhb8a.mongodb.net/?retryWrites=true&w=majority",
 });
